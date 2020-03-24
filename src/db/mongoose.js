fb4cc809cbd9ed2b387f1e7a4de0a7a3c84be0a0
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-03-24 18:20:31 -0400

    set up mongoose connection

---
+++ src/db/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to the DATABASE');
+});
