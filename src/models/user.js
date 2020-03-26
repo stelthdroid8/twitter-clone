ae157f883b6e75fa6103f4e7799f290283ed44ae
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-03-26 16:28:05 -0400

    add jwt token to each user, and generate tokens

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -18,9 +21,30 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     trim: true,
-    minlength: 8
-  }
+    minlength: 6
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
 
+userSchema.methods.generateAuthToken = async function() {
+  const user = this;
+
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_KEY);
+
+  user.tokens = user.tokens.concat({ token });
+
+  await user.save();
+
+  return token;
+};
+
 const User = mongoose.model('User', userSchema);
+
 module.exports = User;
