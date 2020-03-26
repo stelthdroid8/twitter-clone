c181d03e0879e3daa3949c69fe90ba916508edb3
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-03-26 16:44:54 -0400

    add password hashing

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 require('dotenv').config();
 
@@ -55,6 +56,16 @@ userSchema.methods.toJSON = function() {
 
   return userObject;
 };
+
+userSchema.pre('save', async function(next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
 
 const User = mongoose.model('User', userSchema);
 
