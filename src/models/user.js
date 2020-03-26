9221446aa3b9b524e12cc824d03d3138c26ecf30
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-03-26 16:36:49 -0400

    remove password and tokens off server response

--- src/models/user.js
+++ src/models/user.js
@@ -45,6 +45,17 @@ userSchema.methods.generateAuthToken = async function() {
   return token;
 };
 
+userSchema.methods.toJSON = function() {
+  const user = this;
+
+  const userObject = user.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
