0ca5c7989a3a49cd4221dbadb2fbeeb1e222822b
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-03-26 16:26:46 -0400

    add user signup route

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,4 +10,16 @@ router.get('/', (req, res) => {
   res.send('this worked');
 });
 
+router.post('/users/signup', async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 module.exports = router;
