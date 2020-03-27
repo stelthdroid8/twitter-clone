89eff8aa053996037e32ab65d1816521b45c6272
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-03-27 15:58:52 -0400

    allow user to be found by email and verify login credentials

--- src/models/user.js
+++ src/models/user.js
@@ -61,6 +61,21 @@ userSchema.methods.generateAuthToken = async function() {
   return token;
 };
 
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('unable to login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error('unable to login');
+  }
+
+  return user;
+};
 userSchema.methods.toJSON = function() {
   const user = this;
 
