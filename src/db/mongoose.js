c824e23cdadb4e5c68499f7aa9b0f0cd96c76d2d
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-03-27 15:58:28 -0400

    update message

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -11,5 +11,5 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('connected to the DATABASE');
+  console.log('connected to the db');
 });
